2c15dcd61179f4bf7c93b16dfa4372827b167191
esambrailo <e.sambrailo@ischool.berkeley.edu>
2024-11-08 09:30:51 -0800

    imported Entity orchard type

--- python/jumanji/environments/simple_orchard/generator.py
+++ python/jumanji/environments/simple_orchard/generator.py
@@ -2,7 +2,7 @@
 
 ### NEED TO UPDATE FOR OUR CODE ###
 # from our modified files
-from orchard_types import SimpleOrchardApple, SimpleOrchardState
+from orchard_types import SimpleOrchardApple, SimpleOrchardState, SimpleOrchardEntity
 
 from typing import Tuple
 
