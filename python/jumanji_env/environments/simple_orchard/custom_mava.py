a9024bb2909134ab70064e59769ee8d6428b2c2b
esambrailo <e.sambrailo@ischool.berkeley.edu>
2024-11-11 17:14:19 -0800

    Wrapper to replace LbfWrapper

--- python/jumanji_env/environments/simple_orchard/custom_mava.py
+++ python/jumanji_env/environments/simple_orchard/custom_mava.py
@@ -1,17 +1,22 @@
 # this contains modifications to mava functions necessary for us to work with our apple orchard environment. 
 
 # importing packages
+import chex
+import jax.numpy as jnp
 from typing import Tuple
 from omegaconf import DictConfig
 import jumanji
-from mava.types import MarlEnv
+from jumanji.types import TimeStep
+from mava.types import MarlEnv, Observation, State
 from mava.wrappers import (
     RecordEpisodeMetrics
 )
+# pulling in the Jumanji mult-agent wrapper
+from mava.wrappers.jumanji import MultiAgentWrapper
 
 # our custom packages
-from jumanji_env.environments.simple_orchard.generator import SimpleOrchardGenerator
-
+from generator import SimpleOrchardGenerator
+from env import SimpleOrchard
 
 # This function replaces functionality of Mava's `mava.utils.make_env` functionality. 
 # right now this just contains the RecordEpisodeMetrics wrapper and does not contain an equivalent
@@ -47,5 +52,53 @@ def make_env(env_name: str, config: DictConfig) -> Tuple[MarlEnv, MarlEnv]:
     # adding the wrappers that log evaluation metrics.
     train_env = RecordEpisodeMetrics(train_env)
     eval_env = RecordEpisodeMetrics(eval_env)
+    
+    # adding the orchard specific wrapper below
+    train_env = SimpleOrchardWrapper(train_env)
+    eval_env = SimpleOrchardWrapper(eval_env)
 
     return train_env, eval_env
+
+# This function replaces the functionality of Mavas `mava.wrappers.jumanji.LbfWrapper` functionality. 
+# Only update necessary was to change the environment type to SimpleOrchard. 
+class SimpeOrchardWrapper(MultiAgentWrapper):
+    """
+     Multi-agent wrapper for the Level-Based Foraging environment.
+
+    Args:
+        env (Environment): The base environment.
+        use_individual_rewards (bool): If true each agent gets a separate reward,
+        sum reward otherwise.
+    """
+
+    def __init__(self, env: SimpleOrchard, use_individual_rewards: bool = False):
+        super().__init__(env)
+        self._env: SimpleOrchard
+        self._use_individual_rewards = use_individual_rewards
+
+    def aggregate_rewards(
+        self, timestep: TimeStep, observation: Observation
+    ) -> TimeStep[Observation]:
+        """Aggregate individual rewards across agents."""
+        team_reward = jnp.sum(timestep.reward)
+
+        # Repeat the aggregated reward for each agent.
+        reward = jnp.repeat(team_reward, self._num_agents)
+        return timestep.replace(observation=observation, reward=reward)
+
+    def modify_timestep(self, timestep: TimeStep) -> TimeStep[Observation]:
+        """Modify the timestep for Level-Based Foraging environment and update
+        the reward based on the specified reward handling strategy."""
+
+        # Create a new observation with adjusted step count
+        modified_observation = Observation(
+            agents_view=timestep.observation.agents_view,
+            action_mask=timestep.observation.action_mask,
+            step_count=jnp.repeat(timestep.observation.step_count, self._num_agents),
+        )
+        if self._use_individual_rewards:
+            # The environment returns a list of individual rewards and these are used as is.
+            return timestep.replace(observation=modified_observation)
+
+        # Aggregate the list of individual rewards and use a single team_reward.
+        return self.aggregate_rewards(timestep, modified_observation)
