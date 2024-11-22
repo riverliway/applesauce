1a479d5e589044083f739746b7b4833fa1551ca8
esambrailo <e.sambrailo@ischool.berkeley.edu>
2024-11-22 11:04:41 -0800

    trying to address tracer error

--- python/jumanji_env/environments/complex_orchard/generator.py
+++ python/jumanji_env/environments/complex_orchard/generator.py
@@ -80,8 +80,8 @@ class ComplexOrchardGenerator:
     """
     
     # Ensure concrete inputs for tree distances
-    tree_row_distance = float(tree_row_distance)
-    tree_col_distance = float(tree_col_distance)
+    tree_row_distance = tree_row_distance.astype(float)
+    tree_col_distance = tree_col_distance.astype(float)
     
     tree_x = jnp.arange(tree_row_distance, self.width - tree_row_distance, tree_row_distance / 2)
     tree_y = jnp.arange(tree_col_distance * 2, self.height - tree_col_distance / 2, tree_col_distance / 2)
