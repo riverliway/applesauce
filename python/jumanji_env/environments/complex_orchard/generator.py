9852949184751ca5f0fc2c113c760dea3facc8ed
esambrailo <e.sambrailo@ischool.berkeley.edu>
2024-11-22 10:44:51 -0800

    casting tree spacing as floats to address JAX computational issue

--- python/jumanji_env/environments/complex_orchard/generator.py
+++ python/jumanji_env/environments/complex_orchard/generator.py
@@ -246,8 +246,8 @@ class ComplexOrchardGenerator:
     Randomly creates an initial orchard state
     """
 
-    tree_row_distance = self.random_normal(key, (1,), TREE_DISTANCE_ROW)[0]
-    tree_col_distance = self.random_normal(key, (1,), TREE_DISTANCE_COL)[0]
+    tree_row_distance = float(self.random_normal(key, (1,), TREE_DISTANCE_ROW)[0])
+    tree_col_distance = float(self.random_normal(key, (1,), TREE_DISTANCE_COL)[0])
 
     (
       tree_pos_key,
