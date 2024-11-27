09dea65eec209e84f7b75a2fb99266248a0a105d
esambrailo <e.sambrailo@ischool.berkeley.edu>
2024-11-27 12:28:31 -0800

    removing residual print statement

--- python/jumanji_env/environments/complex_orchard/env.py
+++ python/jumanji_env/environments/complex_orchard/env.py
@@ -97,7 +97,6 @@ class ComplexOrchard(Environment[ComplexOrchardState]):
             Tuple[State, TimeStep]: `State` object corresponding to the new initial state
             of the environment and `TimeStep` object corresponding to the initial timestep.
         """
-        print(f"key passed to env.reset: {key}, type: {type(key)}")
         state = self._generator(key)
         observation = self._observer.state_to_observation(state)
         timestep = restart(observation, shape=self.num_agents)
